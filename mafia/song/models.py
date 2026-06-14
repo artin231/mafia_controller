db27f669900a2d2d0b182e21aadbba914803b12c
artin231 <leiartrezjah@gmail.com>
2026-06-15 00:15:25 +0330

    i added the verbose_name for our table and i added a super user for the admin

--- mafia/song/models.py
+++ mafia/song/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Song(models.Model):
-    name = models.CharField(max_length=400)
-    singer = models.CharField(max_length=200)
-    song = models.FileField(upload_to='song')
+    name = models.CharField(max_length=400,verbose_name='اسم آهنگ')
+    singer = models.CharField(max_length=200 , verbose_name='خواننده ی آهنگ')
+    song = models.FileField(upload_to='song' , verbose_name='فایل آهنگ')
+
+    class Meta:
+        verbose_name='آهنگ'
+        verbose_name_plural = 'آهنگ ها'
