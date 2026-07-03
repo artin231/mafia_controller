5d400bcdf3ba799da84e8e6a1a13afe1086eb6e7
artin231 <leiartrezjah@gmail.com>
2026-07-03 14:48:19 +0330

    i maked the /about url

--- mafia/sitesettings/views.py
+++ mafia/sitesettings/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from . import models
+from django.views.generic import View,DetailView
 # Create your views here.
+
+class SiteAboutView(View):
+    def get(request):
+        ActiveAbout = models.Site.objects.filter(True).first()
+
+        
+
+    def post(request):
+        pass
