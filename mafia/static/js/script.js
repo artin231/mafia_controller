92ad55576c1c4f65cdf5b9357cdbb00fe8bef8aa
artin231 <leiartrezjah@gmail.com>
2026-08-11 17:55:30 +0330

    showing alert with sweetalert

--- mafia/static/js/script.js
+++ mafia/static/js/script.js
@@ -16,17 +16,37 @@ $(document).scroll((e) => {
 
 $('#FormComment').submit((e) => {
   e.preventDefault()
-  let ValName = $('#NameInput').val();
-  let ValEmail = $('#EmailInput').val();
-  let ValMessage = $('#MessageInput').val();
-  let data = { name: ValName, email: ValEmail, message: ValMessage }
-  $.get("/api/message", data,
-    function (res, textStatus, jqXHR) {
-      console.log(res);
-      $('#NameInput').val('');
-      $('#EmailInput').val('');
-      $('#MessageInput').val('');
-    }
-  );
+  try {
+    let ValName = $('#NameInput').val();
+    let ValEmail = $('#EmailInput').val();
+    let ValMessage = $('#MessageInput').val();
+    let data = { name: ValName, email: ValEmail, message: ValMessage }
+
+    $.get("/api/message", data,
+      function (res, textStatus, jqXHR) {
+        console.log(res);
+
+        $('#NameInput').val('');
+        $('#EmailInput').val('');
+        $('#MessageInput').val('');
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "عملیات با موفقیت انجام شد",
+          showConfirmButton: false,
+          timer: 1500
+        });
+      }
+    );
+  }
+  catch {
+    Swal.fire({
+      position: "center",
+      icon: "error",
+      title: "عملیات با مشکل مواجه شد",
+      showConfirmButton: false,
+      timer: 1500
+    });
+  }
 })
 
